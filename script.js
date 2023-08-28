7df4ca90be8a5f5bf4a20d344e6d86e966832336
ashutosh9897794237 <113291959+ashutosh9897794237@users.noreply.github.com>
2023-08-29 01:15:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-window.promises = [];
+
 
 // Do not change the code above this
 // add your promises to the array `promises`
